ed24d677e81eadfb8f5f84f9a97a61025d1a90dc
anis25 <aniscseru6125@gmail.com>
2017-09-12 01:38:44 +0600

    Second assignment second part

---
+++ Part2.java
@@ -0,0 +1,30 @@
+
+
+public class Part2
+{
+    public int howMany(String stringa, String stringb){
+        
+        int startIndex = 0,count = 0;
+        int curIndex = stringb.indexOf(stringa, startIndex);
+        while(true){
+            if(curIndex != -1){
+                count++;
+            }
+            else{
+                break;
+            }
+            curIndex = stringb.indexOf(stringa, curIndex + stringa.length());
+        }
+        return count;
+    }
+    
+    public void testHowMany(){
+        String stringa = "GAA";
+        String stringb = "ATGAACGAATTGAATC";
+        System.out.println(howMany(stringa, stringb));
+        stringa = "AA";
+        stringb = "ATAAAA";
+        System.out.println(howMany(stringa, stringb));
+    }
+}
+
